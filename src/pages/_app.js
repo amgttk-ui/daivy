0b6f96ca276001d79d828f55388e0b71501a64f5
amgttk-ui <amgttk@gmail.com>
2026-07-02 04:30:00 +0800

    fix language provider import

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,9 @@
-import "@/styles/globals.css";
+import { LanguageProvider } from "../context/LanguageContext";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <LanguageProvider>
+      <Component {...pageProps} />
+    </LanguageProvider>
+  );
 }
